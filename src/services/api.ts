7c8eb1abaec7977cfa420e0016b830b652ee1ebc
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-09-01 12:37:12 +0200

    Fix typo in Content-Type header
    deleted 1 "a" on row 17 as requested

--- src/services/api.ts
+++ src/services/api.ts
@@ -14,7 +14,7 @@ export async function post<T, U>(endpoint: string, data: U): Promise<T> {
   const response = await fetch(`${BASE_URL}${endpoint}`, {
     method: "POST",
     headers: {
-      "Content-Type": "aaplication/json",
+      "Content-Type": "aplication/json",
     },
     body: JSON.stringify(data),
   });
