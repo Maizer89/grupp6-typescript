280045ace01907863ec89413124b7b4f9a35abed
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2026-09-03 21:02:14 +0200

    feat: display rooms on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,28 @@
+import { useEffect, useState } from "react";
+import type { GroupRoom } from "../types/GroupRoom";
+import { getRooms } from "../services/roomService";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  // Sparar alla hämtade rum
+  const [rooms, setRooms] = useState<GroupRoom[]>([]);
+
+  useEffect(() => {
+    // Hämtar rummen när sidan laddas
+    getRooms()
+      .then((data) => setRooms(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <main>
+      <h1>Tillgängliga rum</h1>
+
+      {rooms.map((room) => (
+        <div key={room.id}>
+          <h2>{room.name}</h2>
+          <p>Kapacitet: {room.capacity} personer</p>
+        </div>
+      ))}
+    </main>
+  );
 }
