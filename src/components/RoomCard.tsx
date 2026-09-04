c140c1f6eabd8d372146a181e06cf93fe8d9c52b
Hosein Moustafa <51028108+r8now@users.noreply.github.com>
2026-09-04 19:28:17 +0200

    feat: link room cards to room details

--- src/components/RoomCard.tsx
+++ src/components/RoomCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import type { GroupRoom } from "../types/GroupRoom";
 
 interface RoomCardProps {
@@ -10,6 +11,9 @@ export default function RoomCard({ room }: RoomCardProps) {
     <div>
       <h2>{room.name}</h2>
       <p>Kapacitet: {room.capacity} personer</p>
+
+      {/* Länkar till detaljsidan för det valda rummet */}
+      <Link to={`/rooms/${room.id}`}>Visa rum</Link>
     </div>
   );
 }
