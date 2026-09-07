aecd8bc9279a98764d8706875f0d841e74eb6dc8
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-09-07 12:38:25 +0200

    Added get for bookings

--- src/services/bookingservice.ts
+++ src/services/bookingservice.ts
@@ -1,8 +1,13 @@
-import { post } from "./api";
+import { get, post } from "./api";
 import type { Booking, NewBooking } from "../types/Booking";
 
 //Tar emot en ny bokning utan id
 export function createBooking(booking: NewBooking): Promise<Booking> {
   //skickar bokning till JSON server på /bookings
   return post<Booking, NewBooking>("/bookings", booking);
+}
+
+//Hämtar alla bokningar
+export function getBookings(): Promise<Booking[]> {
+  return get<Booking[]>("/bookings");
 }
