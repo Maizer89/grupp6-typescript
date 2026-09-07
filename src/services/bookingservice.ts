38735a85cc02679e7c454b79c2779dffc4baf97c
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-09-07 08:55:50 +0200

    Added function to create Booking

---
+++ src/services/bookingservice.ts
@@ -0,0 +1,8 @@
+import { post } from "./api";
+import type { Booking, NewBooking } from "../types/Booking";
+
+//Tar emot en ny bokning utan id
+export function createBooking(booking: NewBooking): Promise<Booking> {
+  //skickar bokning till JSON server på /bookings
+  return post<Booking, NewBooking>("/bookings", booking);
+}
