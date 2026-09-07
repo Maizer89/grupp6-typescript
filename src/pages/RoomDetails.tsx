1f5315425a9966e570ef3cef8cbab279e9794b54
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-09-07 09:44:54 +0200

    Added bookingform to roomdetails

--- src/pages/RoomDetails.tsx
+++ src/pages/RoomDetails.tsx
@@ -1,3 +1,20 @@
+import { useParams } from "react-router";
+import BookingForm from "../components/BookingForm";
+
 export default function RoomDetails() {
-  return <h1>RoomDetails</h1>;
+  const { id } = useParams();
+
+  if (!id) {
+    return <p>Rummet kunde inte hittas, var god försök igen!</p>;
+  }
+
+  const roomId = Number(id);
+
+  return (
+    <main>
+      <h1>Rum detaljer</h1>
+
+      <BookingForm roomId={roomId} />
+    </main>
+  );
 }
