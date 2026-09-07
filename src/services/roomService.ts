40b6a474f91984ef1f6cf3bbc59e7971c1330e9d
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-09-07 09:50:55 +0200

    Added function to only get 1 room by id

--- src/services/roomService.ts
+++ src/services/roomService.ts
@@ -6,3 +6,8 @@ export function getRooms(): Promise<GroupRoom[]> {
   // Returnerar en array med GroupRoom-objekt
   return get<GroupRoom[]>("/rooms");
 }
+
+//Hämtar 1 rum
+export function getRoom(id: number): Promise<GroupRoom> {
+  return get<GroupRoom>(`/rooms/${id}`);
+}
