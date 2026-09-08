c58a36531c71b6010431bd981345648921ee1ccc
Rasmus Billgren <rasmusbillgren89@gmail.com>
2026-09-08 08:50:41 +0200

    Added Handler for undefined room-id

--- src/pages/RoomDetails.tsx
+++ src/pages/RoomDetails.tsx
@@ -8,13 +8,17 @@ export default function RoomDetails() {
   const { id } = useParams();
   const [room, setRoom] = useState<GroupRoom | null>(null);
 
-  const roomId = Number(id);
-
   useEffect(() => {
+    if (id === undefined) {
+      return;
+    }
+
+    const roomId = Number(id);
+
     getRoom(roomId)
       .then((data) => setRoom(data))
       .catch((error) => console.error(error));
-  }, [id, roomId]);
+  }, [id]);
 
   if (!id) {
     return <p>Rummet hittades inte! Var god försök igen</p>;
